e5e507e54e245a5f5c0c1626019e0fc1faa93e08
AhmedEhab88 <ahmedesrm@gmail.com>
2024-12-31 00:06:37 +0300

    Updated app.routes.ts to use separate components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 
-import { IndexComponent } from './product/index/index.component';
-import { ViewComponent } from './product/view/view.component';
-import { EditComponent } from './product/edit/edit.component';
-import { CreateComponent } from './create/create.component';
+import { ProductIndexComponent } from './features/product/product-index/product-index.component';
+import { ProductViewComponent } from './features/product/product-view/product-view.component';
+import { ProductEditComponent } from './features/product/product-edit/product-edit.component';
+import { ProductCreateComponent } from './features/product/product-create/product-create.component';
 
 export const routes: Routes = [
     { path: 'product', redirectTo: 'post/index', pathMatch: 'full' },
-    { path: 'product/index', component: IndexComponent },
-    { path: 'product/:productId/view', component: ViewComponent },
-    { path: 'product/create', component: CreateComponent },
-    { path: 'product/:productId/edit', component: EditComponent },
+    { path: 'product/index', component: ProductIndexComponent },
+    { path: 'product/:productId/view', component: ProductViewComponent },
+    { path: 'product/create', component: ProductCreateComponent },
+    { path: 'product/:productId/edit', component: ProductEditComponent },
 ];
