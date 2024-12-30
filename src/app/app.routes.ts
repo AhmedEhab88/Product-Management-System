431c71d7da27ec0c84fedaab14592af70b356ce5
AhmedEhab88 <ahmedesrm@gmail.com>
2024-12-30 22:20:56 +0300

    Configured routes of web app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { IndexComponent } from './product/index/index.component';
+import { ViewComponent } from './product/view/view.component';
+import { EditComponent } from './product/edit/edit.component';
+import { CreateComponent } from './product/create/create.component';
+
+export const routes: Routes = [
+    { path: 'product', redirectTo: 'post/index', pathMatch: 'full' },
+    { path: 'product/index', component: IndexComponent },
+    { path: 'product/:productId/view', component: ViewComponent },
+    { path: 'product/create', component: CreateComponent },
+    { path: 'product/:productId/edit', component: EditComponent },
+];
