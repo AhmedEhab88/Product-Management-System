24c41914326f4586bc24119fbe4fe390f0d37135
AhmedEhab88 <ahmedesrm@gmail.com>
2024-12-31 21:18:13 +0300

    Using confirmation dialog component

--- src/app/features/product/product-index/product-index.component.ts
+++ src/app/features/product/product-index/product-index.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
-import { Product } from '../../product/product';
-import { ProductService } from '../../../shared/product.service';
+import { Component, ViewChild } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { TableModule } from 'primeng/table';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { Product } from '../../product/product';
+import { ProductService } from '../../../shared/product.service';
+import { ConfirmDialogComponent } from '../../../confirmation-dialog/confirmation-dialog.component';
 
 @Component({
     selector: 'app-product-index',
     standalone: true,
-    imports: [HttpClientModule, TableModule, RouterModule],
+    imports: [HttpClientModule, TableModule, RouterModule, CommonModule, ConfirmDialogComponent],
     providers: [ProductService],
     templateUrl: './product-index.component.html',
     styleUrl: './product-index.component.css',
 })
 export class ProductIndexComponent {
+    @ViewChild(ConfirmDialogComponent) confirmDialog!: ConfirmDialogComponent;
+
     products: Product[] = [];
+    selectedProductId: number = -1;
 
     constructor(public productService: ProductService) {}
 
@@ -25,10 +31,16 @@ export class ProductIndexComponent {
         });
     }
 
+    onDelete(productId: number): void {
+        this.selectedProductId = productId;
+        this.confirmDialog.show('Confirm Deletion', 'Are you sure you want to delete this product?');
+    }
+
     deleteProduct(id: number) {
-        this.productService.delete(id).subscribe((res) => {
-            this.products = this.products.filter((item) => item.id != id);
-            console.log(`Post ${id} deleted successfully `);
-        });
+        console.log(`Post ${id} deleted successfully `);
+        // this.productService.delete(id).subscribe((res) => {
+        //     this.products = this.products.filter((item) => item.id != id);
+        //     console.log(`Post ${id} deleted successfully `);
+        // });
     }
 }
