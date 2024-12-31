ec907c1aad2a99cb6e330c9c0a3a582ea4f3c643
AhmedEhab88 <ahmedesrm@gmail.com>
2025-01-01 00:15:04 +0300

    Implemented constructor for Product

--- Backend/Product-Management-System-API/Product-Management-System-API/Product.cs
+++ Backend/Product-Management-System-API/Product-Management-System-API/Product.cs
@@ -2,6 +2,14 @@
 {
     public class Product
     {
+        public Product(string? name, string? description, decimal price, DateTime createdDate)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            CreatedDate = createdDate;
+        }
+
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
