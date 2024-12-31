93794e70fee9e03d7b33e797149296ee4b5b97e2
Ahmed Ehab <ahmedesrm@gmail.com>
2024-12-31 14:52:27 +0300

    Implemented Product Create component

--- src/app/features/product/product-create/product-create.component.ts
+++ src/app/features/product/product-create/product-create.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ProductService } from '../../../shared/product.service';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-product-create',
-  imports: [],
-  templateUrl: './product-create.component.html',
-  styleUrl: './product-create.component.css'
+    selector: 'app-product-create',
+    standalone: true,
+    imports: [HttpClientModule, CommonModule, ReactiveFormsModule],
+    providers: [ProductService],
+    templateUrl: './product-create.component.html',
+    styleUrls: ['./product-create.component.css'],
 })
 export class ProductCreateComponent {
+    form!: FormGroup;
 
+    constructor(
+        public productService: ProductService,
+        private router: Router,
+    ) {}
+
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            name: new FormControl('', [Validators.required]),
+            description: new FormControl('', [Validators.required]),
+            Price: new FormControl('', [Validators.required]),
+        });
+    }
+
+    get f() {
+        return this.form.controls;
+    }
+
+    submit() {
+        console.log(this.form.value);
+        this.productService.create(this.form.value).subscribe((res: any) => {
+            console.log('Product Created Successfully!');
+            this.router.navigateByUrl('product/index');
+        });
+    }
 }
