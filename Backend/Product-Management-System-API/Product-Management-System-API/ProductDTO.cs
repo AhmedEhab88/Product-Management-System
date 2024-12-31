f39e958aabf11ba4469b6c3fb567b9d3c00de27f
AhmedEhab88 <ahmedesrm@gmail.com>
2025-01-01 00:14:49 +0300

    Created ProductDTO

---
+++ Backend/Product-Management-System-API/Product-Management-System-API/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_Management_System_API
+{
+    public class ProductDTO
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
