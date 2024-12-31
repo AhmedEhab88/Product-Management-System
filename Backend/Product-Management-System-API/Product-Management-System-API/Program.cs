aba3df31918e6b3dd0f64933bd7b3f2a7c9014b8
AhmedEhab88 <ahmedesrm@gmail.com>
2024-12-31 23:01:32 +0300

    Initial backend code commit

---
+++ Backend/Product-Management-System-API/Product-Management-System-API/Program.cs
@@ -0,0 +1,15 @@
+namespace Product_Management_System_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
