3e7ebc590037d9694bc683998ce816ddae5bd0c7
AhmedEhab88 <ahmedesrm@gmail.com>
2024-12-31 18:48:46 +0300

    Removed unnecessary "view" component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 import { ProductIndexComponent } from './features/product/product-index/product-index.component';
-import { ProductViewComponent } from './features/product/product-view/product-view.component';
 import { ProductEditComponent } from './features/product/product-edit/product-edit.component';
 import { ProductCreateComponent } from './features/product/product-create/product-create.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -10,7 +9,6 @@ export const routes: Routes = [
     { path: '', redirectTo: 'products/index', pathMatch: 'full' },
     { path: 'products', redirectTo: 'products/index', pathMatch: 'full' },
     { path: 'products/index', component: ProductIndexComponent },
-    { path: 'products/:productId/view', component: ProductViewComponent },
     { path: 'products/create', component: ProductCreateComponent },
     { path: 'products/:productId/edit', component: ProductEditComponent },
     { path: '**', component: NotFoundComponent },
