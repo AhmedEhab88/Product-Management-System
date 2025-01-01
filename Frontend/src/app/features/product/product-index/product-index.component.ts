b4b93338678b02050e2cb22df6c3be7b153aaa17
Ahmed Ehab <ahmedesrm@gmail.com>
2025-01-01 13:06:57 +0300

    Showing notification messages in case of success & errors

--- Frontend/src/app/features/product/product-index/product-index.component.ts
+++ Frontend/src/app/features/product/product-index/product-index.component.ts
@@ -8,29 +8,47 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../product/product';
 import { ProductService } from '../../../shared/product.service';
 import { ConfirmDialogComponent } from '../../../confirmation-dialog/confirmation-dialog.component';
+import { NotificationComponent } from '../../../shared/notification/notification.component';
 
 @Component({
     selector: 'app-product-index',
     standalone: true,
-    imports: [HttpClientModule, TableModule, RouterModule, CommonModule, ConfirmDialogComponent, InputTextModule],
+    imports: [
+        HttpClientModule,
+        TableModule,
+        RouterModule,
+        CommonModule,
+        ConfirmDialogComponent,
+        InputTextModule,
+        NotificationComponent,
+    ],
     providers: [ProductService],
     templateUrl: './product-index.component.html',
     styleUrl: './product-index.component.css',
 })
 export class ProductIndexComponent {
     @ViewChild(ConfirmDialogComponent) confirmDialog!: ConfirmDialogComponent;
-    @ViewChild('dt2') dt2: Table | undefined; // Access the p-table component
+    @ViewChild('dt2') dt2: Table | undefined;
+    @ViewChild('notification') notification!: NotificationComponent;
 
     products: Product[] = [];
     selectedProductId: number = -1;
+    successMessage: string = '';
 
     constructor(public productService: ProductService) {}
 
     ngOnInit(): void {
-        this.productService.getAll().subscribe((data: Product[]) => {
-            this.products = data;
-            console.log(this.products);
-        });
+        this.productService.getAll().subscribe(
+            (data: Product[]) => {
+                this.products = data;
+                console.log(this.products);
+            },
+            (error) => {
+                this.successMessage = `An error occured when retrieving the products!`;
+                this.notification.color = '#f44336';
+                this.notification.show();
+            },
+        );
     }
 
     onGlobalFilter(event: Event) {
@@ -50,10 +68,20 @@ export class ProductIndexComponent {
     }
 
     deleteProduct(id: number) {
-        console.log(`Post ${id} deleted successfully `);
-        // this.productService.delete(id).subscribe((res) => {
-        //     this.products = this.products.filter((item) => item.id != id);
-        //     console.log(`Post ${id} deleted successfully `);
-        // });
+        this.productService.delete(id).subscribe(
+            (res) => {
+                this.products = this.products.filter((item) => item.id != id);
+                this.successMessage = `Product ${id} deleted successfully!`;
+                this.notification.color = '#4caf50';
+                this.notification.show();
+                console.log(`Post ${id} deleted successfully `);
+            },
+            (error) => {
+                this.successMessage = `Error deleting product ${id}.`;
+                this.notification.color = '#f44336'; // Red for error
+                this.notification.show();
+                console.error(`Error deleting product ${id}`, error);
+            },
+        );
     }
 }
